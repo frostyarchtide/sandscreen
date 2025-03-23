851c0d4e755b39094582cc1282bd71ab75c853da
Frosty <frosty@archtide.space>
2025-03-23 13:58:21 -0400

    Add some optional arguments, very WIP implementation

--- src/main.c
+++ src/main.c
@@ -119,7 +119,24 @@ void sand_grid_display(SandGrid *sand_grid, bool extend_edge) {
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    int spawn_frames_arg = -1;
+    int wait_frames_arg = -1;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "s:w:?")) != -1) {
+        switch (opt) {
+            case '?':
+                return 1;
+            case 's':
+                spawn_frames_arg = atoi(optarg);
+                break;
+            case 'w':
+                wait_frames_arg = atoi(optarg);
+                break;
+        }
+    }
+
     srand(time(NULL) ^ getpid());
 
     setlocale(LC_ALL, "");
@@ -135,11 +152,15 @@ int main() {
     bool extend_edge = width % 2 == 1;
     width /= 2;
 
+    if (spawn_frames_arg == -1) spawn_frames_arg = height;
+    if (wait_frames_arg == -1) wait_frames_arg = height / 4;
+
     SandGrid *sand_grid = sand_grid_create(width, height, false);
 
     bool clearing = false;
-    unsigned int spawn_frames = height;
-    unsigned int wait_frames = 0;
+    printf("%d\n", spawn_frames_arg);
+    int spawn_frames = spawn_frames_arg;
+    int wait_frames = 0;
     while (true) {
         char character = getch();
         if (character != ERR && character == 'q') {
@@ -166,11 +187,11 @@ int main() {
         if (clearing) {
             if (!sand_grid_clear_falling(sand_grid)) {
                 clearing = false;
-                spawn_frames = height;
+                spawn_frames = spawn_frames_arg;
             }
         } else if (!sand_grid_update(sand_grid)) {
             clearing = true;
-            wait_frames = height / 4;
+            wait_frames = wait_frames_arg;
         }
     }
 
